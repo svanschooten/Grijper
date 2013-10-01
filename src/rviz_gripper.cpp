67a210cc80621c8371dec0124e9dedbaf13c0d05
Stijn van Schooten <svsoke@hotmail.com>
2013-10-01 16:50:29 +0200

    inserted kill command into all nodes

--- src/rviz_gripper.cpp
+++ src/rviz_gripper.cpp
@@ -1,17 +1,21 @@
 #include <ros/ros.h>
 #include <string>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
+
 //#include <rvix> en dan nog wat dingen
 
 using namespace std;
 
 void rvizUpdater(const std_msgs::Float32::ConstPtr&);
+void shutdown(const std_msgs::Bool::ConstPtr&);
 
 int main(int argc, char **argv){
 	
 	ros::init(argc, argv, "rviz_gripper");
 	ros::NodeHandle n;
 	ros::Subscriber subscriber = n.subscribe("gripper_state", 1000, rvizUpdater);
+	ros::Subscriber sd = n.subscribe("shutdown", 1, shutdown);
 	ros::spin();
 
 	return 0;
@@ -19,4 +23,8 @@ int main(int argc, char **argv){
 
 void rvizUpdater(const std_msgs::Float32::ConstPtr& msg){
 	ROS_INFO("rziv distance: %f", msg->data);
+}
+
+void shutdown(const std_msgs::Bool::ConstPtr& b){
+	ros::shutdown();
 }
--- src/rviz_object.cpp
+++ src/rviz_object.cpp
@@ -1,15 +1,18 @@
 #include <ros/ros.h>
 #include <string>
 #include <std_msgs/Int32.h>
+#include <std_msgs/Bool.h>
 //#include <rvix> en dan nog wat dingen
 
 void rvizUpdater(const std_msgs::Int32::ConstPtr&);
+void shutdown(const std_msgs::Bool::ConstPtr&);
 
 int main(int argc, char **argv){
 	
 	ros::init(argc, argv, "rviz_object");
 	ros::NodeHandle n;
 	ros::Subscriber subscriber = n.subscribe("phidget_value", 1000, rvizUpdater);
+	ros::Subscriber sd = n.subscribe("shutdown", 1, shutdown);
 	ros::spin();
 
 	return 0;
@@ -17,4 +20,8 @@ int main(int argc, char **argv){
 
 void rvizUpdater(const std_msgs::Int32::ConstPtr& msg){
 	ROS_INFO("rziv distance: %i", msg->data);
+}
+
+void shutdown(const std_msgs::Bool::ConstPtr& b){
+	ros::shutdown();
 }
