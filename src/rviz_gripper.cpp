ada84460851129a672cde33ff5d3a9aa5c635dac
florens <f.kreuk@outlook.com>
2013-10-01 16:58:40 +0200

    branching rviz

--- src/rviz_gripper.cpp
+++ src/rviz_gripper.cpp
@@ -1,22 +1,75 @@
 #include <ros/ros.h>
 #include <string>
 #include <std_msgs/Float32.h>
+#include <sensor_msgs/JointState.h>
 //#include <rvix> en dan nog wat dingen
 
 using namespace std;
 
 void rvizUpdater(const std_msgs::Float32::ConstPtr&);
+float motor_stand;
 
 int main(int argc, char **argv){
 	
 	ros::init(argc, argv, "rviz_gripper");
 	ros::NodeHandle n;
 	ros::Subscriber subscriber = n.subscribe("gripper_state", 1000, rvizUpdater);
-	ros::spin();
+
+	ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("jstates", 1);
+	ros::Rate loop_rate(10);
+	ros::spinOnce();
+
+	// declare const
+	const double bmdll_max = 0.85;
+	const double mdlltp_max = 0.80;
+	// declare variables
+	double bmdll= 0;
+	double mdlltp = 0;
+	double bm_angle = -0.96, mt_angle = -0.3, angle_tot, step_size, cf = 1;
+
+	// calculate step_size and total angle
+	angle_tot = bmdll_max + mdlltp_max*cf;
+	step_size = 1/angle_tot;
+
+	// message declarations
+	sensor_msgs::JointState joint_state;
+
+	while (ros::ok())
+	{
+
+		if(motor_stand <= bmdll_max*step_size)
+		{
+			bmdll = motor_stand/step_size;
+			bm_angle = -0.96+bmdll;
+		}else{
+			mdlltp = (motor_stand/step_size - bmdll_max)/cf;
+			mt_angle = -0.3+mdlltp;
+		}
+
+		joint_state.header.stamp = ros::Time::now();
+		joint_state.name.resize(2);
+		joint_state.position.resize(2);
+
+		joint_state.name[0]="rght_base2mdll";
+		joint_state.position[0] = bm_angle;
+
+		joint_state.name[1]="rght_mdll2tip";
+		joint_state.position[1] = mt_angle;
+
+		// send joint state
+		joint_pub.publish(joint_state);
+
+		ROS_INFO("mt_angle: [%f]", mt_angle);
+
+		//std::cout<<bmdll;
+		ros::spinOnce();
+    	loop_rate.sleep();
+		
+	}
 
 	return 0;
 }
 
 void rvizUpdater(const std_msgs::Float32::ConstPtr& msg){
-	ROS_INFO("rziv distance: %f", msg->data);
+	ROS_INFO("grippere state: %f", msg->data);
 }
--- src/rviz_object.cpp
+++ src/rviz_object.cpp
@@ -1,20 +1,93 @@
 #include <ros/ros.h>
 #include <string>
 #include <std_msgs/Int32.h>
+#include <visualization_msgs/Marker.h>
 //#include <rvix> en dan nog wat dingen
 
 void rvizUpdater(const std_msgs::Int32::ConstPtr&);
+// declare variables 
+int dist_int;
 
 int main(int argc, char **argv){
 	
+	double dist;
+	bool added = false;
+	double diameter = 0.06, height=0.1;;
+
 	ros::init(argc, argv, "rviz_object");
 	ros::NodeHandle n;
 	ros::Subscriber subscriber = n.subscribe("phidget_value", 1000, rvizUpdater);
-	ros::spin();
+	ros::Publisher cylinder_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+	ros::Rate loop_rate(10);
+	ros::spinOnce();
+	
 
+	while (ros::ok())
+  	{
+  		// message
+		visualization_msgs::Marker marker;
+		// marker frame
+		marker.header.frame_id = "base";
+		// timestamp
+		marker.header.stamp = ros::Time::now();
+		// marker name
+		marker.ns="shape_cylinder";
+		// marker id
+		marker.id=0;
+		// marker shape
+		marker.type = visualization_msgs::Marker::CYLINDER;
+		// add marker
+
+    	if(dist_int)
+    	{
+    		// distance 2 double
+			dist = dist_int/100;
+
+			// add marker if there is no marker
+    		if(added==false){
+				marker.action = visualization_msgs::Marker::ADD;
+				added = true;
+			}
+    		
+			//marker position
+			marker.pose.position.x = 0;
+			marker.pose.position.y = -dist;
+			marker.pose.position.z = height/2-0.02;
+			// marker orientation
+			marker.pose.orientation.x = 0.0;
+			marker.pose.orientation.y = 0.0;
+			marker.pose.orientation.z = 0.0;
+			marker.pose.orientation.w = 1.0;
+
+			// scale shape
+			marker.scale.x = diameter;
+			marker.scale.y = diameter;
+			marker.scale.z = height;
+
+			// color
+			marker.color.r = 0.0f;
+			marker.color.g = 1.0f;
+			marker.color.b = 0.0f;
+			marker.color.a = 1.0;
+
+			// lifetime
+			marker.lifetime = ros::Duration();
+
+   			cylinder_pub.publish(marker);
+    	}else if(added == true){
+    		// if there is no object in the range of the sensor delete the marker
+    		marker.action = visualization_msgs::Marker::DELETE;
+			added = false;
+			cylinder_pub.publish(marker);
+    	}
+    	ros::spinOnce();
+    	loop_rate.sleep();
+  	}
+	
 	return 0;
 }
 
 void rvizUpdater(const std_msgs::Int32::ConstPtr& msg){
+	dist_int = msg->data;
 	ROS_INFO("rziv distance: %i", msg->data);
 }
