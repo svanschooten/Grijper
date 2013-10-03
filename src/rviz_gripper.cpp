5fdb9694c0db6bb45afbbfec364df7b37ac00f90
florens <f.kreuk@outlook.com>
2013-10-03 15:32:21 +0200

    rviz_gripper and rviz_object updated to show the gripper state correctly in rviz rviz description file added. Grijper.launch rviz_gripper and rviz_object uncommented

--- src/rviz_gripper.cpp
+++ src/rviz_gripper.cpp
@@ -9,7 +9,29 @@ using namespace std;
 
 void rvizUpdater(const std_msgs::Float32::ConstPtr&);
 void shutdown(const std_msgs::Bool::ConstPtr&);
-float opens;
+float opens; /*!< The state of the gripper 0 when gripper closes, 1 when gripper opens, 0.5 in relax mode. */
+const int freq = 250; /*!< The frequency at which gripper in rviz is updated. */
+
+/*! \brief Main method reads if the state of the gripper is changing and simulates this in RVIZ.
+
+This node listens to the gripper_state published by gripper_actuator. 
+motor_stand variable has a value between 1 and 0. 1 means completly open 0 means completly closed.
+bmdll_max angle that the joint between middle and base can make.
+mlltp_max angle that the joint between middle and finger tip can make.
+bmdll_start start angle of the joint between middle and base
+mlltp_start start angle of the joint between middle and tip
+bm_angle current angle between base and middle
+mt_angle current angle between middle and tip
+step_size the change of motor_stand per loop
+cf corection facter the factor the raid at which the tip moves faster then the middle part
+angle_step the inverse of the total angle that can be made by the two joints
+
+The motor_stand is only changed if the gripper opens or closes and the motor state is between 1 and 0
+The new motor_stand is converted to angles for the both joints.
+The new angles will be published in the jstates topic
+
+\sa shiftBuffer(int), mean()
+*/
 
 int main(int argc, char **argv){
 	
@@ -19,7 +41,7 @@ int main(int argc, char **argv){
 	ros::Subscriber sd = n.subscribe("shutdown", 1, shutdown);
 	ros::spinOnce();
 	ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("jstates", 1);
-	ros::Rate loop_rate(100);
+	ros::Rate loop_rate(freq);
 	ros::spinOnce();
 
 	// declare const
@@ -28,7 +50,7 @@ int main(int argc, char **argv){
 	// declare variables
 	const double bmdll_start = -0.96;
 	const double mdlltp_start = -0.3;
-	double bm_angle, mt_angle, step_size = 0.01, cf = 1, motor_stand = 0;
+	double bm_angle, mt_angle, step_size = 0.004, cf = 1.5, motor_stand = 0;
 
 	// calculate step_size and total angle
 	double angle_step = 1/(bmdll_max + mdlltp_max*cf);
@@ -37,22 +59,21 @@ int main(int argc, char **argv){
 	sensor_msgs::JointState joint_state;
 
 	while (ros::ok())
-	{
-
-		if((opens == 0 || motor_stand == 1)&&(motor_stand+step_size<1 && motor_stand-step_size>0)){
+	{	
+		if((opens == 1 && motor_stand < (1-step_size)) || (opens == 0 && motor_stand > step_size)){
 			if(opens == 0){
-				//gripper closes
-				motor_stand = motor_stand - step_size;
+					motor_stand = motor_stand - step_size;
+				
 			}else{
-				// gripper opens
-				motor_stand = motor_stand + step_size;
+					motor_stand = motor_stand + step_size;
+				
 			}
-			if(motor_stand*angle_step < bmdll_max){
+			if((motor_stand/angle_step) < bmdll_max){
 				mt_angle = mdlltp_start;
-				bm_angle = motor_stand*angle_step;
+				bm_angle = motor_stand/angle_step+bmdll_start;
 			}else{
 				bm_angle = bmdll_start+bmdll_max;
-				mt_angle = (motor_stand*angle_step)-bmdll_max;
+				mt_angle = (motor_stand/angle_step)-bmdll_max+mdlltp_start;
 			}
 			joint_state.header.stamp = ros::Time::now();
 			joint_state.name.resize(2);
@@ -63,14 +84,8 @@ int main(int argc, char **argv){
 
 			joint_state.name[1]="rght_mdll2tip";
 			joint_state.position[1] = mt_angle;
-
-			// send joint state
 			joint_pub.publish(joint_state);
 		}
-
-		//ROS_INFO("mt_angle: [%f]", mt_angle);
-
-		//std::cout<<bmdll;
 		ros::spinOnce();
     	loop_rate.sleep();
 		
@@ -79,10 +94,21 @@ int main(int argc, char **argv){
 	return 0;
 }
 
+/*! \brief update method the opens variable will be updated
+
+\param msg contains the value 0,1,0.5 depending if the gripper is openening, closing or relaxing.
+*/
+
 void rvizUpdater(const std_msgs::Float32::ConstPtr& msg){
 	opens = msg->data;
 	ROS_INFO("open or close: %f", msg->data);
 }
+
+/*! \brief Controll method to shutdown this ROS node when the command is given.
+
+This method will shutdown this node when the command is given by the console.
+\param b The message containing the command. Nothing is done with the command since we know what it will be when this message is received.
+*/
 
 void shutdown(const std_msgs::Bool::ConstPtr& b){
 	ros::shutdown();
--- src/rviz_object.cpp
+++ src/rviz_object.cpp
@@ -8,93 +8,97 @@
 
 void rvizUpdater(const std_msgs::Float32::ConstPtr&);
 void shutdown(const std_msgs::Bool::ConstPtr&);
+ros::Publisher cylinder_pub;
 
-// declare variables 
-float distance;
+//const int freq = 250; /*!< The frequency at which the subscriber reads the . */
+bool added = false; /*!< Cylinder added value */
+
+/*! \brief Main method of the rviz_object node. This node evaluates the distance published by phidget_reader.
+
+If there is an object in sight of the sensor this nod publishes a marker at the distance measured by the sensor in rviz. 
+If there is no object in the sight of the sensor the cylinder will be deleted.
+This node also listens to the shutdown command given by the controller.
+*/
+
 
 int main(int argc, char **argv){
-	
-	bool added = false;
-	double diameter = 0.06, height=0.1;;
 
 	ros::init(argc, argv, "rviz_object");
 	ros::NodeHandle n;
 	ros::Subscriber subscriber = n.subscribe("phidget_value", 1000, rvizUpdater);
 	ros::Subscriber sd = n.subscribe("shutdown", 1, shutdown);
-	ros::spinOnce();
-	ros::Publisher cylinder_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-	ros::Rate loop_rate(10);
-	ros::spinOnce();
+	cylinder_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+	ros::spin();
 	
-
-	while (ros::ok())
-  	{
-  		// message
-		visualization_msgs::Marker marker;
-		// marker frame
-		marker.header.frame_id = "base";
-		// timestamp
-		marker.header.stamp = ros::Time::now();
-		// marker name
-		marker.ns="shape_cylinder";
-		// marker id
-		marker.id=0;
-		// marker shape
-		marker.type = visualization_msgs::Marker::CYLINDER;
-		// add marker
-
-    	if(distance)
-    	{
-
-			// add marker if there is no marker
-    		if(added==false){
-				marker.action = visualization_msgs::Marker::ADD;
-				added = true;
-			}
-    		
-			//marker position
-			marker.pose.position.x = 0;
-			marker.pose.position.y = -distance/100;
-			marker.pose.position.z = height/2-0.02;
-			// marker orientation
-			marker.pose.orientation.x = 0.0;
-			marker.pose.orientation.y = 0.0;
-			marker.pose.orientation.z = 0.0;
-			marker.pose.orientation.w = 1.0;
-
-			// scale shape
-			marker.scale.x = diameter;
-			marker.scale.y = diameter;
-			marker.scale.z = height;
-
-			// color
-			marker.color.r = 0.0f;
-			marker.color.g = 1.0f;
-			marker.color.b = 0.0f;
-			marker.color.a = 1.0;
-
-			// lifetime
-			marker.lifetime = ros::Duration();
-
-   			cylinder_pub.publish(marker);
-    	}else if(added == true){
-    		// if there is no object in the range of the sensor delete the marker
-    		marker.action = visualization_msgs::Marker::DELETE;
-			added = false;
-			cylinder_pub.publish(marker);
-    	}
-    	ros::spinOnce();
-    	loop_rate.sleep();
-  	}
 	
 	return 0;
 }
 
+/*! \brief Method for publishing a cylinder in rviz
+
+This Method creates a marker message to create a cylinder in rviz.
+The frame where the marker in will be plotted in rviz is the base frame of the gripper.
+The marker will be deleted if there is no object in the range of the sensor.
+The type of the marker is a cylinder. Marker will be placed on the y-axis on the measured distance from the origin
+The size of the marker is determined with the diameter and height variable.
+The color of the marker is green.
+*/
 
 void rvizUpdater(const std_msgs::Float32::ConstPtr& msg){
+	const double diameter = 0.06, height=0.1;;
+	float distance;
 	distance = msg->data;
-	ROS_INFO("rziv distance: %f", msg->data);
+	ROS_INFO("rziv distance: %f", distance);
+	
+	visualization_msgs::Marker marker;
+	marker.header.frame_id = "base";
+	marker.header.stamp = ros::Time::now();
+	marker.ns="shape_cylinder";
+	marker.id=0;
+	marker.type = visualization_msgs::Marker::CYLINDER;
+	if(distance == 0){
+		if(added == true){
+			marker.action = visualization_msgs::Marker::DELETE;
+			cylinder_pub.publish(marker);
+		}
+	}else{
+    	if(added==false){
+			marker.action = visualization_msgs::Marker::ADD;
+			added = true;
+		}
+    
+		//marker position
+		marker.pose.position.x = 0;
+		marker.pose.position.y = -distance/100;
+		marker.pose.position.z = height/2-0.02;
+		// marker orientation
+		marker.pose.orientation.x = 0.0;
+		marker.pose.orientation.y = 0.0;
+		marker.pose.orientation.z = 0.0;
+		marker.pose.orientation.w = 1.0;
+
+		// scale shape
+		marker.scale.x = diameter;
+		marker.scale.y = diameter;
+		marker.scale.z = height;
+		// color
+		marker.color.r = 0.0f;
+		marker.color.g = 1.0f;
+		marker.color.b = 0.0f;
+		marker.color.a = 1.0;
+
+		// lifetime
+		marker.lifetime = ros::Duration();
+
+		cylinder_pub.publish(marker);
+	}
 }
+
+/*! \brief Controll method to shutdown this ROS node when the command is given.
+
+This method will shutdown this node when the command is given by the console.
+\param b The message containing the command. Nothing is done with the command since we know what it will be when this message is received.
+*/
 
 void shutdown(const std_msgs::Bool::ConstPtr& b){
 	ros::shutdown();
